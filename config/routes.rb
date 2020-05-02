573d3d5b2bfb1bef69306086faa5ce4b93093439
github YumaTwinBrothers <y_m_yolo@yahoo.co.jp>
2020-05-02 23:11:49 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "pages#home"
+  get 'pages/home', to: 'pages#home'
+
+  resources :blogs do
+    resources :comments, only: [:create]
+    member do
+      post 'like'
+    end
+  end
+
+  get '/signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  mount ActionCable.server => '/cable'
+  get '/chat', to: 'chatrooms#show'
+
+  resources :messages, only: [:create]
 end
