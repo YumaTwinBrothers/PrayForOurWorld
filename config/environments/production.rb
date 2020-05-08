a8ce95f6aa8ae5afae305d3b74e4b2b26b996a3e
github YumaTwinBrothers <y_m_yolo@yahoo.co.jp>
2020-05-09 00:31:29 +0900

    Set the environment for redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.web_socket_server_url = "wss://pray-for-our-world.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://pray-for-our-world.herokuapp.com', 'http://pray-for-our-world.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://pray-for-our-world.herokuapp.com']
 end
