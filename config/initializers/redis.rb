718ee88e4249cc4a6c091da13e51b8dbec7f81e5
github YumaTwinBrothers <y_m_yolo@yahoo.co.jp>
2020-05-08 23:17:47 +0900

    Set the environment for redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,2 @@
-$redis = Redis.new(url: ENV["REDIS_URL"]) if Rails.env.production?
+# $redis = Redis.new(url: ENV["REDIS_URL"]) if Rails.env.production?
+REDIS = Redis.new(host: ENV.fetch("REDIS_HOST") { "cache" }, port: ENV.fetch("REDIS_PORT") { "6379" }, db: ENV.fetch("REDIS_DB") { "0" })
