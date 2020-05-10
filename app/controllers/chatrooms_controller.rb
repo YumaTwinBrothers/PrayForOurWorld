2891e6c5cb90968c1bad3d065ea256ab696c9b28
github YumaTwinBrothers <y_m_yolo@yahoo.co.jp>
2020-05-10 17:40:08 +0900

    Add pagination on chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,7 +2,8 @@ class ChatroomsController < ApplicationController
   before_action :require_user
   def show
     @message = Message.new
-    @messages = Message.most_recent
+    messages = Message.most_recent
+    @messages = messages.paginate(page: params[:page], per_page: 10)
   end
 
 end
